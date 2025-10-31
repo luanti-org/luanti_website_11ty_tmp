81a12e1d48efd8680bc165cbc779508ff321605f
rubenwardy <rw@rubenwardy.com>
2025-10-31 18:00:31 +0000

    Add redirects.json

---
+++ content/redirects.11ty.js
@@ -0,0 +1,18 @@
+export default class Redirects {
+	data() {
+		return {
+			permalink: "/redirects.json",
+			eleventyExcludeFromCollections: true,
+		};
+	}
+
+	render(eleventy) {
+		const redirects = eleventy.collections.all.flatMap(page => {
+			const redirect_from = page.data.redirect_from ?? [];
+			const urls = Array.isArray(redirect_from) ? redirect_from : [redirect_from];
+			return urls.map(url => [ url, this.absolute_url(page.url)]);
+		});
+
+		return JSON.stringify(Object.fromEntries(redirects));
+	}
+}
